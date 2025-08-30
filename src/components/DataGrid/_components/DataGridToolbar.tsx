edb8d25c6dd331f79cdba04ed090a1861139d901
Peter Burdette <peterburdette@gmail.com>
2025-08-30 10:28:56 -0400

    updated padding in toolbar search field

--- src/components/DataGrid/_components/DataGridToolbar.tsx
+++ src/components/DataGrid/_components/DataGridToolbar.tsx
@@ -132,13 +132,12 @@ const DataGridToolbar: React.FC<DataGridToolbarProps> = (props) => {
         {!hideSearch && (
           <div className="relative hidden md:block">
             <div className="relative">
-              <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
               <Input
                 startIcon={<Search className="h-5 w-5" />}
-                placeholder="Search..."
+                placeholder="Search…"
                 value={searchQuery}
                 onChange={(e) => onSearchChange(e.target.value)}
-                className="w-64 pl-9"
+                className="w-64"
                 variant="default"
               />
             </div>
